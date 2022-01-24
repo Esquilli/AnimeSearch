c35c6f6eb3286e5a7ccbcd0459d57aa39e9beda9
Pedro J. Fernandez <esquillidev@gmail.com>
2022-01-24 16:23:33 -0500

    Initial Commit

---
+++ AnimeSearch/AnimeSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeSearchApp.swift
+//  AnimeSearch
+//
+//  Created by Pedro Fernandez on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimeSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
